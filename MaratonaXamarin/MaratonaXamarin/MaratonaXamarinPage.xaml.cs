062c088038418a66a1174ac042f1302795ec87d0
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2017-05-07 13:28:42 -0300

    Cap. 01

---
+++ MaratonaXamarin/MaratonaXamarin/MaratonaXamarinPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MaratonaXamarin
+{
+	public class Veiculo
+	{
+		public string Nome { get; set; }
+		public decimal Preco { get; set; }
+	}	
+
+	public partial class MaratonaXamarinPage : ContentPage
+	{
+		public List<Veiculo> Veiculos { get; set; }
+
+		public MaratonaXamarinPage()
+		{
+			InitializeComponent();
+
+			this.Veiculos = new List<Veiculo>
+			{
+				new Veiculo { Nome = "Azera V6", Preco = 60000 },
+				new Veiculo { Nome = "Fiesta 2.0", Preco = 50000 },
+				new Veiculo { Nome = "Azera V6", Preco = 40000 },
+			};
+
+			this.BindingContext = this;
+		}
+	}
+}
